a9c4cfe5fbe80c06228b4b165129d1c06fcda3af
jgoriginal <932274204@qq.com>
2022-11-15 14:10:12 +0800

    安装 element-plus

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="login">
-    123
-  </div>
+  <div>login</div>
+  <!-- <div class="login">
+    <div class="login-form">
+      <el-form
+    ref="formRef"
+    :model="formData"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+  >
+    <el-form-item label="用户名" prop="username">
+      <el-input v-model="formData.username" />
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input
+        v-model="formData.password"
+        type="password"
+        show-password
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)"
+        >Submit</el-button
+      >
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
+    </div>
+  </div> -->
 </template>
 <script setup>
-
+// import { reactive } from 'vue'
+// // 表单对象
+// const formData = reactive({
+//   username: 'super-admin',
+//   password: 123456
+// })
+// // 校验规则
+// const rules = {}
 </script>
 <style lang="less" scoped>
+.login {
 
+}
 </style>
