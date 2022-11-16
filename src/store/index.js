655bc51409de82c7c2c6b0391d543d28dc109331
jgoriginal <932274204@qq.com>
2022-11-16 12:12:16 +0800

    创建对 userInfo的快捷访问

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,9 @@ export default createStore({
     // 对常规用变量的一些快捷访问
     token (state) {
       return state.user.token
+    },
+    userInfo (state) {
+      return state.user.userInfo
     }
   },
   mutations: {
