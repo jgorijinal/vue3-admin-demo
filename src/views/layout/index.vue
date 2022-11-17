bf89b32f5081d99ab267da0d11d12d8c7493cc83
jgoriginal <932274204@qq.com>
2022-11-18 00:36:55 +0800

    渲染左侧菜单

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -3,27 +3,27 @@
     <el-container class="container">
       <el-aside width="200px">
         <!--菜单-->
-        <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-      >
-        <el-menu-item index="2">
-          <component :is="'location'"></component>
-          <span>Navigator Two</span>
-        </el-menu-item>
-
-        <el-sub-menu index="1">
-          <template #title>
-            <!-- <el-icon><location /></el-icon> -->
+        <el-menu :default-active="defaultActive" class="el-menu-vertical-demo" router>
+          <el-menu-item index="/profile">
             <component :is="'location'"></component>
-            <span>Navigator One</span>
+            <span>profile</span>
+          </el-menu-item>
+          <template v-for="value,key in map" :key="key">
+            <el-sub-menu :index="value.path">
+            <template #title>
+              <!-- <el-icon><location /></el-icon> -->
+              <component :is="value.meta.icon"></component>
+              <span>{{value.meta.title}}</span>
+            </template>
+            <template v-for="item,index in value.children" :key="index">
+              <el-menu-item :index="item.path" v-if="item.meta.icon">
+                <component :is="item.meta.icon"></component>
+                <span>{{item.meta.title}}</span>
+              </el-menu-item>
+            </template>
+          </el-sub-menu>
           </template>
-            <el-menu-item index="1-4-1">
-              <component :is="'location'"></component>
-              <span>item one</span>
-            </el-menu-item>
-        </el-sub-menu>
-      </el-menu>
+        </el-menu>
       </el-aside>
       <el-container>
         <el-header>
@@ -31,14 +31,16 @@
             <!--dropdown 下拉菜单-->
             <el-dropdown>
               <span class="el-dropdown-link">
-                {{$store.getters.userInfo.username}}
+                {{ $store.getters.userInfo.username }}
                 <el-icon class="el-icon--right">
                   <arrow-down />
                 </el-icon>
               </span>
               <template #dropdown>
                 <el-dropdown-menu>
-                  <el-dropdown-item @click="logoutClick">退出登录</el-dropdown-item>
+                  <el-dropdown-item @click="logoutClick"
+                    >退出登录</el-dropdown-item
+                  >
                 </el-dropdown-menu>
               </template>
             </el-dropdown>
@@ -54,11 +56,37 @@
 <script setup>
 import { ArrowDown } from '@element-plus/icons-vue'
 import { useStore } from 'vuex'
+import { computed, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 const store = useStore()
-// 点击 退出登录
+// 点击 退出登出
 const logoutClick = () => {
   store.dispatch('user/logoutAction')
 }
+
+const map = computed(() => {
+  const filterRoutes = store.state.permission.routes.filter(
+    (route) => route.meta
+  )
+  const obj = {}
+  filterRoutes.forEach((route) => {
+    let value = obj[route.path]
+    if (!value) {
+      value = {}
+      obj[route.path] = value
+      value.children = []
+    }
+    value.path = route.path
+    value.meta = route.meta
+    value.children.push(...route.children)
+  })
+  return obj
+})
+const route = useRoute()
+const defaultActive = ref('')
+watch(route, (newRoute) => {
+  defaultActive.value = newRoute.path
+}, { immediate: true })
 </script>
 <style lang="less" scoped>
 .common-layout {
@@ -71,8 +99,8 @@ const logoutClick = () => {
     }
   }
 }
-  svg {
-    width:18px;
-    margin:4px;
-  }
+svg {
+  width: 18px;
+  margin: 4px;
+}
 </style>
