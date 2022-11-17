0e119ec98a71260413ca287c5c1986ac3685c43e
jgoriginal <932274204@qq.com>
2022-11-17 12:51:25 +0800

    UserManage 路由

---
+++ src/router/modules/UserManage.js
@@ -0,0 +1,31 @@
+import layout from '@/views/layout/index.vue'
+export default {
+  path: '/user',
+  name: 'userManage',
+  component: layout,
+  children: [
+    {
+      path: '/user/manage',
+      component: () => import('@/views/user-manage/index.vue'),
+      meta: {
+        title: 'userManage',
+        icon: 'memo'
+      }
+    },
+    {
+      path: '/user/info/:id',
+      name: 'userInfo',
+      component: () => import('@/views/user-info/index.vue'),
+      meta: {
+        title: 'userInfo'
+      }
+    },
+    {
+      path: '/user/import',
+      component: () => import('@/views/import/index.vue'),
+      meta: {
+        title: 'import'
+      }
+    }
+  ]
+}
