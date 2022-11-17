634df7c32264ce83c2cfabb39e02e103b521f741
jgoriginal <932274204@qq.com>
2022-11-17 22:15:40 +0800

    next(to.path)

--- src/permission.js
+++ src/permission.js
@@ -13,15 +13,15 @@ router.beforeEach(async (to, from, next) => {
       if (JSON.stringify(store.getters.userInfo) === '{}') {
         // 获取用户资料
         const userInfo = await store.dispatch('user/getUserInfoAction')
-        console.log(userInfo.permission.menus)
 
         const filterRoutes = await store.dispatch('permission/filterRoutes', userInfo.permission.menus)
-        console.log(filterRoutes)
         filterRoutes.forEach(route => {
           router.addRoute(route)
         })
+        next(to.path)
+      } else {
+        next()
       }
-      next()
     }
   } else {
     if (whiteList.includes(to.path)) {
--- src/store/permission.js
+++ src/store/permission.js
@@ -1,4 +1,4 @@
-// 页面权限模块
+// 页面权限模块}
 import { asyncRoutes, publicRoutes } from '@/router'
 export default {
   namespaced: true,
@@ -8,25 +8,24 @@ export default {
     }
   },
   mutations: {
-    setRoutes (state, routes) {
-      state.routes = [...publicRoutes, ...routes]
+    setRoutes (state, newRoutes) {
+      state.routes = [...publicRoutes, ...newRoutes]
     }
   },
   actions: {
     filterRoutes (context, menus) {
-      const routes = [] // 需要动态添加的路由对象
-      console.log(menus)
-      menus.forEach(menu => {
-        routes.push(asyncRoutes.filter(routeObj => routeObj.name === menu)[0])
+      // 筛选出来的私有路由
+      const routes = []
+      menus.forEach((menu) => {
+        routes.push(asyncRoutes.filter(route => route.name === menu)[0])
       })
-      // 最后要加上这些 /:catchAll(.*) 这个要最后加上
+      // 在 routes 最后面添加 404
       routes.push({
-        path: '/:catchAll(.*)',
+        path: '/catchAll(.*)',
         redirect: '/404'
       })
-      console.log(routes)
       context.commit('setRoutes', routes)
-      // 最后要 return 返回, 因为在前置守卫里面用 addRoute 添加到路由表里面
+      // 最后返回
       return routes
     }
   }
